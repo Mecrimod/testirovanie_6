3254c6767e5d4f22044ace7f45b2e2d34d9d8406
Mecrimod <124774932+Mecrimod@users.noreply.github.com>
2025-11-30 14:34:56 +0300

    Update unit_converter.cpp

--- unit_converter.cpp
+++ unit_converter.cpp
@@ -39,14 +39,14 @@ private:
     map<string, Conversion, std::less<>> length_conversions;
     map<string, Conversion, std::less<>> info_conversions;
 
-    const double getInput(const string& prompt) {
+    double const getInput(const string& prompt) {
         double value;
         cout << prompt;
         cin >> value;
         return value;
     }
 
-    const bool confirmInput() {
+    bool const confirmInput() {
         char confirmation;
         cout << "Вы подтверждаете данные? Введите символ (y), если да, если нет - n: ";
         cin >> confirmation;
